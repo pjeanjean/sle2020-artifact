46cb66680cecb2e4efebfff2f09189e34c003e78
Fabien Coulon <fabien.coulon@obeo.fr>
2020-09-17 06:30:49 +0200

    update

--- setup.sh
+++ setup.sh
@@ -23,6 +23,12 @@ curl -Lo ./java11.tar.gz https://download.java.net/java/GA/jdk11/9/GPL/openjdk-1
 tar -xzf java11.tar.gz
 echo export PATH="~/java/jdk-11/bin:\$PATH" >> ~/.bashrc
 
+# INSTALL MAVEN
+cd
+curl -Lo maven.tar.gz http://apache.crihan.fr/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+tar -xzf maven.tar.gz
+echo export PATH="~/apache-maven-3.6.3/bin:\$PATH" >> ~/.bashrc
+
 # INSTALL DOCKER
 sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 sudo dnf install -y docker-ce docker-ce-cli containerd.io
